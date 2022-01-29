03b4026775615aaa81f28bc15e238cea4c6a3cfc
Hélène Lebrun <helene.lebrun@beslogic.com>
2022-01-29 16:28:37 -0500

    faire le tir de 3 fleches

---
+++ BIJ_GameJam_2/Assets/Player/Scripts/SpecialItems/ThreeArrowsItem.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Player.Scripts.SpecialItems
+{
+    public class ThreeArrowsItem : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            var otherBow = other.GetComponentInChildren<Bow>();
+
+            otherBow.BoosterTime += 5.0f;
+        }
+    }
+}
